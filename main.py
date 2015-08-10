c1d7f056dcf8b26919f924dae5e3c2af408341e6
Asbjørn Steinskog <asbjorn@steinskog.me>
2015-08-10 23:29:39 +0200

    Write to static json file instead of using Tornado

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-import tornado.escape
-import tornado.ioloop
-import tornado.web
 from parsing import *
 
-class PostHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.set_header('Content-Type', 'application/json')
-        self.write(response)
+with open('rating.json', 'w') as out:
+    out.write(response)
 
-application = tornado.web.Application([
-    (r"/", PostHandler),
-])
-
-if __name__ == "__main__":
-    application.listen(8888)
-    tornado.ioloop.IOLoop.instance().start()
