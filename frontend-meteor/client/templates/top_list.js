32a80d57e90c65fdde0e5f7352bc7154769b4239
Asbjørn Steinskog <asbjorn@steinskog.me>
2015-09-02 00:10:20 +0200

    Get top players from backend

--- frontend-meteor/client/templates/top_list.js
+++ frontend-meteor/client/templates/top_list.js
@@ -1,3 +1,15 @@
+Template.topList.onRendered(function() {
+
+	Meteor.call('getTop', function(err, response) {
+		if(err) {
+			console.log("An error occurred retrieving data", err );
+		} else {
+			console.log(_.values(response)[0]);
+			Session.set("top_list", response);
+		}
+	});
+});
+
 Template.topList.helpers({
 	getTop: function() {
 		HTTP.get(Meteor.absoluteUrl("http://vg.no"), function(err,result) {
---
+++ frontend-meteor/server/methods.js
@@ -0,0 +1,16 @@
+Meteor.methods({
+	getTop: function() {
+		var url = "http://localhost:8888/top?limit=10";
+
+		var result = Meteor.http.get(url, {timeout:30000});
+			if(result.statusCode==200) {
+				var respJson = JSON.parse(result.content);
+				console.log("response received.");
+				return respJson;
+			} else {
+				console.log("Response issue: ", result.statusCode);
+				var errorJson = JSON.parse(result.content);
+				throw new Meteor.Error(result.statusCode, errorJson.error);
+			}
+	}
+});
