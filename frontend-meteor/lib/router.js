7b5cddc7f9e76e3d84964a51b62fc42b65b14747
Asbjørn Steinskog <asbjorn@steinskog.me>
2015-09-02 02:23:45 +0200

    Add link to player page and pass id

--- frontend-meteor/lib/router.js
+++ frontend-meteor/lib/router.js
@@ -3,3 +3,10 @@ Router.configure({
 });
 
 Router.route('/', {name: 'frontPage'});
+
+Router.route('player/:_id', {
+    name: 'player',
+    data: function(){
+    return {_id: this.params._id};
+    }
+});
