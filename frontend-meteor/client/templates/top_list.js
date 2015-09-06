249f2c80498314b1ee16731cce1a068dda6d3c2c
Asbjørn Steinskog <asbjorn@steinskog.me>
2015-09-07 01:33:36 +0200

    Fetch from MongoDB instead from API

--- frontend-meteor/client/templates/top_list.js
+++ frontend-meteor/client/templates/top_list.js
@@ -1,47 +1,5 @@
 Template.topList.onRendered(function() {
 
-	Meteor.call('getTop', 'elo', 'MF', function(err, response) {
-		if(err) {
-			console.log("An error occurred retrieving data", err );
-		} else {
-			list = _.values(response)[0];
-
-			for(var i = 0; i<list.length; i++) {
-				list[i].place = i+1;
-			}
-			
-			Session.set('topElo', list);
-		}
-	});
-
-	Meteor.call('getTop', 'number_of_games', 'MF', function(err, response) {
-		if(err) {
-			console.log("An error occurred retrieving data", err );
-		} else {
-			list = _.values(response)[0];
-
-			for(var i = 0; i<list.length; i++) {
-				list[i].place = i+1;
-			}
-
-			Session.set('topGames', list);
-		}
-	});
-
-	Meteor.call('getTop', 'elo', 'F', function(err, response) {
-		if(err) {
-			console.log("An error occurred retrieving data", err );
-		} else {
-			list = _.values(response)[0];
-
-			for(var i = 0; i<list.length; i++) {
-				list[i].place = i+1;
-			}
-
-			Session.set('femaleTopElo', list);
-		}
-	});
-
 	Meteor.call('getStats', 'stats', function(err, response) {
 		if(err) {
 			console.log("An error occurred retrieving data", err );
@@ -52,16 +10,16 @@ Template.topList.onRendered(function() {
 });
 
 Template.topList.helpers({
-	topElo: function(number) {
-		return Session.get('topElo').slice(0, number);
+	topElo: function() {
+		return Players.find({}, {sort: {elo: -1}});
 	},
 
 	topGames: function(number) {
-		return Session.get('topGames').slice(0, number);
+		return Players.find({}, {sort: {number_of_games: -1}});
 	},
 
 	topWomen: function() {
-		return Session.get('femaleTopElo');
+		return Players.find({ gender: "F" }, {sort: {elo: -1}});
 	},
 
 	stats: function() {
--- frontend-meteor/server/methods.js
+++ frontend-meteor/server/methods.js
@@ -1,21 +1,6 @@
 var URL_PREFIX = "http://localhost:8888"
 
 Meteor.methods({
-	getTop: function(argument, gender) {
-		// argument could e.g. be elo or number_of_games
-		var url = URL_PREFIX + "/top?arg=" + argument + "&gender=" + gender;
-
-		var result = Meteor.http.get(url, {timeout:3000});
-			if(result.statusCode==200) {
-				var response = JSON.parse(result.content);
-				console.log("response received.");
-				return response;
-			} else {
-				console.log("Response issue: ", result.statusCode);
-				var errorJson = JSON.parse(result.content);
-				throw new Meteor.Error(result.statusCode, errorJson.error);
-			}
-	},
 
 	getDate: function() {
 		var url = URL_PREFIX + "/date";
