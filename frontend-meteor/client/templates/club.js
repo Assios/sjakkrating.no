5b6366b6c196d291b5d1950e72443b14a01556ec
Asbjørn Steinskog <asbjorn@steinskog.me>
2015-09-14 01:11:45 +0200

    Add gender ratio

--- frontend-meteor/client/templates/club.js
+++ frontend-meteor/client/templates/club.js
@@ -5,4 +5,25 @@ Template.clubPage.helpers({
 			return player;
 		});
 	},
+
+	number_of_players: function(c_name) {
+		return Players.find({club: c_name}).count();
+	},
+
+	average_rating: function(c_name) {
+		total = 0
+
+		Players.find({club: c_name}).map(function(p) {
+		  total += p.elo;
+		});
+
+		return Math.round(total/Players.find({club: c_name}).count());
+	},
+
+	gender_ratio: function(c_name) {
+		male = Players.find({club: c_name, gender: 'M'}).count();
+		female = Players.find({club: c_name, gender: 'F'}).count();
+
+		return (female/male*100).toFixed(2);
+	}
 });
