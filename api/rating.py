cc09713d78a6a98cae3aa87d6acb21e17357fe6b
Asbjørn Steinskog <asbjorn@steinskog.me>
2015-09-24 11:37:27 +0200

    Remove unnecessary code

--- api/rating.py
+++ api/rating.py
@@ -3,10 +3,6 @@
 
 rating_files = ["2002jan", "2002apr", "2002jul", "2003jan", "2003mar", "2003jun", "2003sep", "2004jan", "2004mai", "2004jun", "2004okt", "2005jan", "2005apr", "2005jun", "2005okt", "2006jan", "2006apr", "2006jun", "2006okt", "2007jan", "2007mar", "2007jun", "2007sep", "2008jan", "2008apr", "2008jun", "2008sep", "2009jan", "2009apr", "2009jun", "2009sep", "2010jan", "2010apr", "2010jun", "2010sep", "2011jan", "2011apr", "2011jun", "2011sep", "2012jan", "2012apr", "2012jun", "2012sep", "2013jan", "2013apr", "2013jun", "2013sep", "2014jan", "2014apr", "2014jun", "2014sep", "2015jan", "2015apr", "2015jun", "2015sep"]
 
-file_name = "ratingtall/" + rating_files[0] + ".txt"
-
-f = open(file_name)
-
 class RatingGraphObject:
     def __init__(self, line):
         self.full_name = line[6:32].strip()
@@ -14,13 +10,6 @@ class RatingGraphObject:
         self.temp_elo = line[54:61].strip().split(' ')[0]
         self.fide_elo = line[64:69].strip()
 
-player = RatingGraphObject(f.readlines()[5])
-
-dictplayer = player.__dict__
-
-dictplayer["elos"] = {}
-
-dictplayer["elos"][rating_files[0]] = dictplayer["temp_elo"]
 
 def get_ratings_by_name(full_name):
   elo_dict = {}
@@ -57,3 +46,4 @@ def get_ratings_by_name(full_name):
         elo_dict["fide_elos"].append(int(dictplayer['fide_elo']))             
 
   return elo_dict["categories"], elo_dict["elos"], elo_dict["fide_elos"]
+
