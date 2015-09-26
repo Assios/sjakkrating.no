ad6dc8de8edc2e72fae72d25ee17f6dfa3ddad1f
Asbjørn Steinskog <asbjorn@steinskog.me>
2015-09-26 17:33:04 +0200

    Convert rating file names to year and month This allows for graphing with irregular time intervals.

--- api/rating.py
+++ api/rating.py
@@ -13,6 +13,28 @@ class RatingGraphObject:
         self.blitz_elo = line[74:79].strip()
 
 
+def filename_to_date(filename):
+  months = {
+    "jan": 1,
+    "feb": 2,
+    "mar": 3,
+    "apr": 4,
+    "may": 5,
+    "jun": 6,
+    "jul": 7,
+    "aug": 8,
+    "sep": 9,
+    "okt": 10,
+    "nov": 11,
+    "des": 12,
+  }
+
+  year = int(filename[:4])
+  month = months[filename[4:]]
+
+  return [year, month]
+
+
 def get_ratings_by_name(full_name):
   elo_dict = {}
   elo_dict["elos"] = []
