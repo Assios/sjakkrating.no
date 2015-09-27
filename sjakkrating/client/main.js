005bb3f5b8e7e95def6fdf3a05831db9cf732709
Asbjørn Steinskog <asbjorn@steinskog.me>
2015-09-28 01:53:58 +0200

    Add blitz and rapid elo to profile

--- sjakkrating/client/main.js
+++ sjakkrating/client/main.js
@@ -4,6 +4,8 @@ Highcharts.setOptions({
   lang: {
     loading: "Laster...",
     noData: "Du må velge minst én ratingtype.",
-    resetZoom: "Zoom ut"
+    resetZoom: "Zoom ut",
+    shortMonths: ["Jan" , "Feb" , "Mar" , "Apr" , "Mai" , "Jun" , "Jul" , "Aug" , "Sep" , "Okt" , "Nov" , "Des"],
+    months: ["Januar" , "Februar" , "Mars" , "April" , "Mai" , "Juni" , "Juli" , "August" , "September" , "Oktober" , "November" , "Desember"]
   }
 });
