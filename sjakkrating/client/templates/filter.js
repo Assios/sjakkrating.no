13e0c584fb40799b9f025ef90d3d1787182b59ba
Asbjørn Steinskog <asbjorn@steinskog.me>
2015-09-29 02:02:31 +0200

    Add advanced search

--- sjakkrating/client/templates/filter.js
+++ sjakkrating/client/templates/filter.js
@@ -1,28 +1,34 @@
+Template.filter.onRendered(function() {
+
+  $("#chessTable").stupidtable();
+
+});
+
 Template.filter.helpers({
   filteredPlayers: function() {
     if (Session.get("filter_object"))
-      return Players.find(Session.get("filter_object"), {sort: {elo: -1}, limit: 10});
+      return Players.find(Session.get("filter_object"), {sort: {elo: -1}, limit: Session.get("result_limit")});
     else
       return [];
   }
 });
 
 Template.filter.events({
-  'click .club-submit': function () {
+  'click .advanced-search': function () {
     chess_club = $("#chessClub").val();
     min_rating = parseInt($("#minRating").val());
     max_rating = parseInt($("#maxRating").val());
     min_year_of_birth = parseInt($("#minYearOfBirth").val());
     max_year_of_birth = parseInt($("#maxYearOfBirth").val());
+    number_of_results = parseInt($("#numberOfResults").val());
 
     obj = { nsf_elo: { $gt: min_rating, $lt: max_rating }, year_of_birth: { $gt: min_year_of_birth, $lt: max_year_of_birth } }
 
-    console.log(obj);
-
-    if (chess_club) {
+    if (chess_club && (chess_club!="Alle")) {
       $.extend(obj, { club: chess_club });
     }
 
+    Session.set("result_limit", number_of_results);
     Session.set("filter_object", obj);
   }
 });
