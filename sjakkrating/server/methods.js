f4c2c262399e86ec50fdaf8a5af58edfa4f87a7b
Asbjørn Steinskog <asbjorn@steinskog.me>
2015-10-07 21:25:37 +0200

    Add rating distribution graph

--- sjakkrating/server/methods.js
+++ sjakkrating/server/methods.js
@@ -16,6 +16,21 @@ Meteor.methods({
 				throw new Meteor.Error(result.statusCode, errorJson.error);
 			}
 	},
+
+	getDistribution: function() {
+		var url = URL_PREFIX + "/distribution";
+
+		var result = Meteor.http.get(url, {timeout:3000});
+			if(result.statusCode==200) {
+				var response = JSON.parse(result.content);
+				console.log("response received.");
+				return response;
+			} else {
+				console.log("Response issue: ", result.statusCode);
+				var errorJson = JSON.parse(result.content);
+				throw new Meteor.Error(result.statusCode, errorJson.error);
+			}		
+	},
 /*
 	getPlayer: function(nsf_id) {
 		var url = URL_PREFIX + "/player/" + nsf_id;
