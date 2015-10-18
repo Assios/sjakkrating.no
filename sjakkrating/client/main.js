3b22b5e74f1c12f71db19fc0c5bc52c08128c8bf
Asbjørn Steinskog <asbjorn@steinskog.me>
2015-10-18 19:54:46 +0200

    Small fixes

--- sjakkrating/client/main.js
+++ sjakkrating/client/main.js
@@ -1,9 +1,7 @@
-Meteor.subscribe('clubs')
-
 Highcharts.setOptions({
   lang: {
     loading: "Laster...",
-    noData: "Du må velge minst én ratingtype.",
+    noData: "Ingen data tilgjengelig.",
     resetZoom: "Zoom ut",
     shortMonths: ["Jan" , "Feb" , "Mar" , "Apr" , "Mai" , "Jun" , "Jul" , "Aug" , "Sep" , "Okt" , "Nov" , "Des"],
     months: ["Januar" , "Februar" , "Mars" , "April" , "Mai" , "Juni" , "Juli" , "August" , "September" , "Oktober" , "November" , "Desember"]
---
+++ sjakkrating/client/templates/auto_complete_players.js
@@ -0,0 +1,13 @@
+Template.autoCompletePlayers.helpers({
+    labelBasedOnRatingDifference: function(newRating, oldRating) {
+        if (newRating < oldRating) {
+            return "label label-danger";
+        }
+        else if (newRating > oldRating) {
+            return "label label-success";
+        }
+        else {
+            return "label label-default";
+        }
+    },
+});
